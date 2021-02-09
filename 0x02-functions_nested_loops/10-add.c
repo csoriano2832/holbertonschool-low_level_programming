5e9d85df9b32ff1c20e0118e1cc689230cabb898
Christopher Soriano <2832@holbertonschool.com>
2021-02-09 17:20:50 +0000

    Add file 8 and 10 & modify header file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - add two integers
+ * @x: first number to sum
+ * @y: second number to sum
+ *
+ * Return: result
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
