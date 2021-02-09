7f8936aea500d418a758ab72772f0aa180ec1d27
Christopher Soriano <2832@holbertonschool.com>
2021-02-09 11:36:09 +0000

    Remove raw file & Change content of file 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
   * main - entry point
-  * Return: always success (0)
+  * Return: always 0 (success)
   */
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
