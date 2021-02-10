ff9ab2dd8f2dd362836f5edfdbb04d2e6ffeaa4c
Christopher Soriano <2832@holbertonschool.com>
2021-02-10 20:13:17 +0000

    Add file 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of the first 32 fibonacci numbers that are even
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	long a = 0;
+	long b = 1;
+	long fib = 1;
+	long sum = fib;
+
+	for (i = 1; i <= 32; i++)
+	{
+		fib = a + b;
+		a = b;
+		b = fib;
+		if (fib % 2 == 0)
+		{
+			sum += fib;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
