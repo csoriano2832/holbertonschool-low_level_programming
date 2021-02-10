34e607c2db37dfdd109c80c7e1143679172756b9
Christopher Soriano <2832@holbertonschool.com>
2021-02-10 03:24:51 +0000

    Add file 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int firstDigit, secondDigit;
+
+	for (firstDigit = 0; firstDigit <= 99; firstDigit++)
+	{
+		for (secondDigit = 0; secondDigit <= 99; secondDigit++)
+		{
+			if ((firstDigit != secondDigit) && (firstDigit < secondDigit))
+			{
+				putchar((firstDigit / 10) + '0');
+				putchar((firstDigit % 10) + '0');
+				putchar(' ');
+				putchar((secondDigit / 10) + '0');
+				putchar((secondDigit % 10) + '0');
+				if (!(firstDigit == 98  && secondDigit == 99 ))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
