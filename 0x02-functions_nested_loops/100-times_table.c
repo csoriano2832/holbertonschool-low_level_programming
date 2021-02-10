80be5ae9950d4e7c2568bb14be1a785f98be0052
Christopher Soriano <2832@holbertonschool.com>
2021-02-10 15:25:43 +0000

    Encapsulate formats in functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 
+void three_digit_format(int);
+void two_digit_format(int);
+void one_digit_format(int, int);
+
 /**
- * print_times_table - prints various times tables
+ * print_times_table - prints times tables
  * @width: size of the table
  *
  * Return: null
@@ -19,24 +23,15 @@ void print_times_table(int width)
 				result = row * column;
 				if (result > 99)
 				{
-					_putchar(' ');
-					_putchar((result / 100) + '0');
-					_putchar(((result % 100) / 10) + '0');
-					_putchar((result % 10) + '0');
+					three_digit_format(result);
 				}
 				else if (result > 9)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
+					two_digit_format(result);
 				}
 				else
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
+					one_digit_format(row, result);
 				}
 				if (row != width)
 				{
@@ -47,4 +42,51 @@ void print_times_table(int width)
 			_putchar('\n');
 		}
 	}
+}
+
+/**
+ * three_digit_format - prints three digit number separated by one space
+ * @result: the number to be printed
+ *
+ * Return: null
+ */
+void three_digit_format(int result)
+{
+	_putchar(' ');
+	_putchar((result / 100) + '0');
+	_putchar(((result % 100) / 10) + '0');
+	_putchar((result % 10) + '0');
+}
+
+/**
+ * two_digit_format - prints two digit number separated by two spaces
+ * @result: the number to be printed
+ *
+ * Return: null
+ */
+void two_digit_format(int result)
+{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar((result / 10) + '0');
+	_putchar((result % 10) + '0');
+}
+
+/**
+ * one_digit_format - prints one digit number separated by three spaces
+ * no space is printed if it's the first number on the table.
+ * @row: number of the row
+ * @result: the number to be printed
+ *
+ * Return: null
+ */
+void one_digit_format(int row, int result)
+{
+	if (row != 0)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	_putchar(result + '0');
 }
