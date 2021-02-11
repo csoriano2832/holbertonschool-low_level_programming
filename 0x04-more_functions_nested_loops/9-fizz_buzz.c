0eee826115937d30152f2ed3d1e26990635d1742
Christopher Soriano <2832@holbertonschool.com>
2021-02-11 20:03:44 +0000

    Remove trailing whitespace

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,9 +16,9 @@ int main(void)
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
 		else if (i % 5 == 0)
-			if (i ==100)
+			if (i == 100)
 				printf("Buzz");
-			else 
+			else
 				printf("Buzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
