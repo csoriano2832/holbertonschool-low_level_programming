db448dd08dca104722ed18acb49e52f71e24b00e
Christopher Soriano <2832@holbertonschool.com>
2021-02-17 02:08:55 +0000

    Fix logic errors

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,23 +8,19 @@
  */
 void puts_half(char *str)
 {
-	int i, len, n;
+	int i = 0, len = 0, n;
 
-	len = n = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
+	while (str[i++])
 		len++;
-	}
 
-	if ((len % 2) != 0)
-		n = (len - 1) / 2;
-	else
+	if ((len % 2) == 0)
 		n = len / 2;
 
-	for (str += n; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
