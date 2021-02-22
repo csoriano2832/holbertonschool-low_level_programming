63c6468556b96ea771eb0a15fd14a413a8e6c651
Christopher Soriano <2832@holbertonschool.com>
2021-02-22 22:06:03 +0000

    Fix logic error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,11 @@
   */
 char *_strchr(char *s, char c)
 {
-	while (*s != c)
+	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		s++;
 	}
-
-	if (*s == c)
-		return (s);
-	else
-		return ('\0');
+	return ('\0');
 }
