177eb2b57b493199a580e64b48001c85b147d8b8
Christopher Soriano <2832@holbertonschool.com>
2021-02-22 22:22:11 +0000

    Add break to while statement

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,14 +5,17 @@
   * @s: the string that will be evaluated
   * @c: the character to be located in the string
   * Return: the pointer to the string
-  */
+ */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != c)
 	{
-		if (*s == c)
-			return (s);
 		s++;
+		if (*s == '\0')
+			break;
 	}
-	return ('\0');
+	if (*s == c)
+		return (s);
+	else
+		return ('\0');
 }
