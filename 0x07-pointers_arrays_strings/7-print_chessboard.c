ab895c37855c13487cc57e9a4fabb322057113fb
Christopher Soriano <2832@holbertonschool.com>
2021-02-23 15:11:38 +0000

    First commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,19 @@
 #include "holberton.h"
 
 /**
-  * print_chessboard - prints a chessboard
-  * @a: pointer to a char
-  */
+ * print_chessboard - prints a chessboard
+ * @a: pointer to a char
+ */
 void print_chessboard(char (*a)[8])
 {
+	int row, col;
 
+	for (col = 0; col < 8; col++)
+	{
+		for (row = 0; row < 8; row++)
+		{
+			_putchar(a[col][row]);
+		}
+		_putchar('\n');
+	}
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,30 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
-  * print_diagsums - prints the sum of the two diagonals of a square matrix
-  * @a: a pointer to a two dimensional arrayu
-  * @size: the quantity of integers to be summed
-  */
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
+ * @a: a pointer to a two dimensional array
+ * @size: the quantity of integers to be summed
+ */
 void print_diagsums(int *a, int size)
 {
+	int idx, sum1, sum2;
 
+	sum1 = 0;
+	sum2 = 0;
+
+	for (idx = 0; idx < size; idx++)
+	{
+		sum1 += a[((size + 1) * idx)];
+		idx++;
+	}
+
+	idx = 0;
+	while (idx < size)
+	{
+		sum2 += a[((size - 1) * (idx + 1))];;
+		idx++;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
