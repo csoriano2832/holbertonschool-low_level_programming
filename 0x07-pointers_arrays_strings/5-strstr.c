ce4c23fd103b2aa5fe381087dc6585b99f587b8d
Christopher Soriano <2832@holbertonschool.com>
2021-02-23 13:49:35 +0000

    Fix logic errors

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,32 @@
 #include "holberton.h"
 
 /**
-  * _strstr - locates a substring
-  * @haystack: a string
-  * @needle: a substring to look within haystack
-  * Return: a pointer to the beginning of the located substring
-  * NULL if substring not found
-  */
+ * _strstr - locates a substring
+ * @haystack: a string
+ * @needle: a substring to look within haystack
+ * Return: a pointer to the beginning of the located substring
+ * NULL if substring not found
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	char *start;
+	char *match;
+	int idx;
 
 	if (*needle == '\0')
 		return (haystack);
 
-	start = needle;
 	while (*haystack)
 	{
-		needle = start;
-		while (*needle == *haystack)
+		if (*needle == *haystack)
 		{
-			needle++;
-			haystack++;
+			match = haystack;
+
+			for (idx = 0; needle[idx] == *haystack; idx++)
+				haystack++;
+
+			if (needle[idx] == '\0')
+				return (match);
 		}
-		if (*needle == '\0')
-			return (haystack);
 		haystack++;
 	}
 	return ('\0');
