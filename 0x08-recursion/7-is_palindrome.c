4a46633824a7a89e7be3a07233857cfec5e9b550
Christopher Soriano <2832@holbertonschool.com>
2021-02-25 02:47:04 +0000

    First commit

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+
+/**
+ * palindrome_check - check for palindrome
+ * @s: reverse string
+ * @i: normal string
+ *
+ * Return: 1 if it's palindrome 0 if it's not
+ */
+int palindrome_check(char *s, char *i)
+{
+	if (*s != *i)
+		return (0);
+
+	if (*s == *i)
+		palindrome_check(s - 1, i + 1);
+	return (1);
+
+}
+
+/**
+ * palindrome_mover - reverses string
+ * @s: reverse string
+ * @i: normal string
+ *
+ * Return: 1 if it's palindrome 0 if it's not
+ */
+int palindrome_mover(char *s, char *i)
+{
+	int count = 0;
+
+	if (*s == '\0')
+		palindrome_mover(s + 1, i);
+
+	count = count + palindrome_check(s - 1, i);
+	return (count);
+}
+
+/**
+ * is_palindrome - check for palindrome
+ * @s: reverse string
+ *
+ * Return: 1 if it's palindrome 0 if it's not
+ */
+int is_palindrome(char *s)
+{
+	char *i;
+
+	i = s;
+
+	if (*s == '\0')
+		return (1);
+
+	return (palindrome_mover(s, i));
+}
