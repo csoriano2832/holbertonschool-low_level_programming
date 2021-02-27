6bf35f9d5e7b1ad08574f57c5b582801dc03e605
Christopher Soriano <2832@holbertonschool.com>
2021-02-27 21:30:10 +0000

    Add betty doc and fix possible edge cases

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,13 +3,17 @@
 #include <ctype.h>
 
 /**
+ * main - add positive numbers
+ * @argc: numbers of command line arguments passed to it
+ * @argv: pointer to string array containing the arguments
  *
- *
- *
+ * Return: 0 on success
+ * Otherwise, return 1 on error.
  */
 int main(int argc, char *argv[])
 {
-	int idx, result;
+	char *str;
+	int idx, jdx, result = 0;
 
 	if (argc == 1)
 		printf("0\n");
@@ -18,13 +22,16 @@ int main(int argc, char *argv[])
 	{
 		for (idx = 1; idx < argc; idx++)
 		{
-			if (!isdigit(*argv[idx]))
+			str = argv[idx];
+			for (jdx = 0; str[jdx] != '\0'; jdx++)
 			{
-				printf("Error\n");
-				return (1);
+				if (!isdigit(str[jdx]))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-
-			result += atoi(argv[idx]);
+			result += atoi(str);
 		}
 		printf("%d\n", result);
 	}
