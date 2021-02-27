fa40c22f09e39ca665acd4b0b4d869e00e6da9ba
Christopher Soriano <2832@holbertonschool.com>
2021-02-27 19:40:23 +0000

    Add betty doc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * main - prints the name of the program
+ * @argc: number of command line arguments
+ * @argv: pointer to char array containing the arguments passed
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
