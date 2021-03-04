7eaa795d0934506cec01c79963c7291baca02959
Christopher Soriano <2832@holbertonschool.com>
2021-03-04 19:34:16 +0000

    Remove trailing whitespace

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	new_arr = malloc(sizeof(int) * (diff + 1));
 	if (new_arr == NULL)
 		return (NULL);
-	
+
 	for (idx = 0; min <= max; idx++)
 	{
 		new_arr[idx] = min;
