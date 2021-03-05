158f641847c99ac6c15c9e524a8216bf894a46c6
Christopher Soriano <2832@holbertonschool.com>
2021-03-05 00:13:45 +0000

    Add betty doc and remove trailing whitespace

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
 
 /**
+  * _realloc - reallocates a memory block using malloc and free
+  * @ptr: pointer to an array
+  * @old_size: size in bytes of old array
+  * @new_size: size in bytes of new array
   *
-  *
-  *
+  * Return: pointer to a new array
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -28,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 		return (new_ptr);
 	}
-	
+
 	new_ptr = malloc(new_size);
 	free(ptr);
 	return (new_ptr);
