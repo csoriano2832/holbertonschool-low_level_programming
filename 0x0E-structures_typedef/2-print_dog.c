48d0c734141909baaf94a2172dc90a97caf3dc9c
Christopher Soriano <2832@holbertonschool.com>
2021-03-08 18:42:31 +0000

    Add edge cases on task 1 and 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,20 +11,20 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		exit(EXIT_SUCCESS);
+		return;
 
 	if (d->name == NULL)
-		d->name = "(nil)";
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", d->name);
 
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-
-	printf("Name: %s\n", d->name);
-
-	if (d->age == 0)
+	if (d->age == 0.0)
 		printf("Age: (nil)");
 	else
 		printf("Age: %f\n", d->age);
 
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
 }
