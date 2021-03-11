e7d493e17352e9a916ebad488ae6d8f42df99136
Christopher Soriano <2832@holbertonschool.com>
2021-03-11 21:39:23 +0000

    Add prototypes to header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -24,5 +24,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void type_string(va_list *arg);
+void type_float(va_list *arg);
+void type_int(va_list *arg);
+void type_char(va_list *arg);
 
 #endif
