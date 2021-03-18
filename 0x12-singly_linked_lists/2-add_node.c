8f089e485154fe86078841364ff595950e7940cc
csoriano2832 <2832@holbertonschool.com>
2021-03-18 19:53:01 +0000

    Move _strlen func to header file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-int _strlen(const char *str);
 /**
  * add_node - adds a new node at the beginning of a list
  * @head: the first element of a list
@@ -11,12 +10,13 @@ int _strlen(const char *str);
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	
+	if (str == NULL)
+		return (NULL);
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 
 	new->str = strdup(str);
 	new->len = _strlen(str);
