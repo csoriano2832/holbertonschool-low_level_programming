e7df17c352b60c5fd8a18a6216f6e1ac2339fb1d
csoriano2832 <2832@holbertonschool.com>
2021-03-18 19:44:29 +0000

    Add file task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+int _strlen(const char *str);
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head: the first element of a list
+ * @str: the string to be inserted
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * _strlen - returns the length of a given string
+ * @str: a string
+ *
+ * Return: number of characters in the string
+ */
+int _strlen(const char *str)
+{
+	int i, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	return (len);
+}
