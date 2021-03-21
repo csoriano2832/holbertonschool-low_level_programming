5fd89b8c141ee4de9d4ab8f64e292c62f9633ad8
csoriano2832 <2832@holbertonschool.com>
2021-03-21 14:22:02 +0000

    Add task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void beforeMain(void) __attribute__ ((constructor));
+
+void beforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
