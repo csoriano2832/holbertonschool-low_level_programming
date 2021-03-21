6d0dbfb687a9196aeea06bfb961a23c6fc4824ff
csoriano2832 <2832@holbertonschool.com>
2021-03-21 15:02:09 +0000

    Fix task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,3 @@
-	.file   "101-hello_holberton.c"
-	.section        .rodata
 .LC0:
 	.string "Hello, Holberton"
 	.text
@@ -20,8 +18,3 @@ main:
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE0:
-	.size   main, .-main
-	.ident  "GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
-	.section        .note.GNU-stack,"",@progbits
-
