43144bf96a7c72da04bc3e46921bf52f2b383727
csoriano2832 <2832@holbertonschool.com>
2021-03-21 16:32:24 +0000

    Add task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a list
+  * @head: the first element of a list
+  *
+  * Return: nothing
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+}
