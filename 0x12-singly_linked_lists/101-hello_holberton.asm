0f4b74aea6764416bd99aed995a4835c47ffd3f5
csoriano2832 <2832@holbertonschool.com>
2021-03-22 18:26:44 +0000

    Update task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,10 @@
 .LC0:
 	.string "Hello, Holberton"
-	.text
-	.globl  main
-	.type   main, @function
 main:
-	.LFB0:
-	.cfi_startproc
 	pushq   %rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
 	movq    %rsp, %rbp
-	.cfi_def_cfa_register 6
 	movl    $.LC0, %edi
 	call    puts
 	movl    $0, %eax
 	popq    %rbp
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
