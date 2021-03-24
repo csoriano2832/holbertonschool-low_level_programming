0482e6d9f6a9dca3a07c2ed2d9553beef2b32f4f
csoriano2832 <2832@holbertonschool.com>
2021-03-24 14:54:57 +0000

    Add task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,16 @@
-.LC0:
-	.string "Hello, Holberton"
+section .data
+	message db "Hello, Holberton",10
+
+section .text
+	global main
+
 main:
-	pushq   %rbp
-	movq    %rsp, %rbp
-	movl    $.LC0, %edi
-	call    puts
-	movl    $0, %eax
-	popq    %rbp
-	ret
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
