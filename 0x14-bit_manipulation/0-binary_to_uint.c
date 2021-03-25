db5c83a2b7c1d70d6baf98582bc5ff379f3066d3
csoriano2832 <2832@holbertonschool.com>
2021-03-25 16:23:24 +0000

    Replace recursion with bitwise operator

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,8 +11,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int index, length = 0;
-	unsigned int decimal = 0, exponent = 0;
-	unsigned int base = 2;
+	unsigned int decimal = 0, exponent = 0, bit = 0;
 
 	if (b == NULL)
 		return (0);
@@ -28,28 +27,12 @@ unsigned int binary_to_uint(const char *b)
 	for (length = length - 1; length >= 0; length--)
 	{
 		if (b[length] == '1')
-		{
-			decimal += _pow_recursion(base, exponent);
-		}
+			bit = 1;
+		else if (b[length] == '0')
+			bit = 0;
+
+		decimal += bit << exponent;
 		exponent++;
 	}
 	return (decimal);
-}
-
-/**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: a number
- * @y: an exponent
- * Return: On success the result of x raised to y
- * -1 on error
- */
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-
-	if (y == 0)
-		return (1);
-
-	return (x = x * _pow_recursion(x, y - 1));
 }
