5d40007d36729c9efe26fdf0db22e6b2b0a40a8f
csoriano2832 <2832@holbertonschool.com>
2021-03-30 17:28:35 +0000

    Add file 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: a file
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, index;
+	size_t count = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (fd == -1)
+		return (-1);
+
+	index = 0;
+	while (text_content[index])
+	{
+		index++;
+		count++;
+	}
+
+	write(fd, text_content, count);
+
+	close(fd);
+	return (1);
+}
