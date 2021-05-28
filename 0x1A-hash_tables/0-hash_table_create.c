b7ec16a7de20f19877bdde8bdadd8b5a1510244a
csoriano2832 <2832@holbertonschool.com>
2021-05-28 14:48:30 +0000

    [Fix] betty errors on header file, initialize pointer to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,15 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *ht = NULL;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-	{
 		return (NULL);
-	}
 
 	ht->array = malloc(sizeof(void *) * size);
 	if (ht->array == NULL)
-	{
 		return (NULL);
-	}
 
 	return (ht);
 }
