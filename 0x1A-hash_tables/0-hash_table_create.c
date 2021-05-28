4085639c70f143b6db8a4ff1de3f2ed7740ddc7f
csoriano2832 <2832@holbertonschool.com>
2021-05-28 15:08:36 +0000

    [Fix] change calloc back to malloc, handle size=0 edge case, initialize all hash table variables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,15 +8,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int index = 0;
 	hash_table_t *ht = NULL;
+
+	if (size == 0)
+		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
-	ht->array = calloc(size, sizeof(void *));
+	ht->array = malloc(sizeof(void *) * size);
 	if (ht->array == NULL)
 		return (NULL);
+
+	ht->size = size;
+
+	for (; index < size; index++)
+	{
+		ht->array[index] = NULL;
+	}
 
 	return (ht);
 }
