81f873a77a60bc6eb6d4a1600104ef2c2b939252
csoriano2832 <2832@holbertonschool.com>
2021-05-29 03:56:17 +0000

    Add code to file

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table
+ */
+void hash_table_delete(hash_table_t *ht) 
+{
+	unsigned long int index;
+	hash_node_t *current_node, *temp_node;
+
+	if (ht == NULL)
+		return;
+
+
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[index] != NULL)
+		{
+			current_node = ht->array[index];
+			while (current_node != NULL)
+			{
+				temp_node = current_node->next;
+				free(current_node->value);
+				free(current_node->key);
+				free(current_node);
+				current_node = temp_node;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
