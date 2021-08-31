2cc35c57284d6eb961a7878c5345e51f85f87613
csoriano2832 <2832@holbertonschool.com>
2021-08-31 17:33:58 +0000

    separate print to its own function

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,29 @@
 #include "search_algos.h"
 
 /**
+ * print_binary_search - This function prints an array within set boundaries.
+ *
+ * @array: is a pointer to the first element in the array
+ * @L: is the leftmost set boundary
+ * @R: is the rightmost set boundary
+ *
+ */
+void print_binary_search(int *array, int L, int R)
+{
+	if (L <= R)
+	{
+		printf("Searching in array: ");
+		for (; L <= R; L++)
+		{
+			printf("%i", array[L]);
+			if (L != R)
+				printf(", ");
+		}
+		printf("\n");
+	}
+}
+
+/**
  * binary_search - This function searches for a value in a sorted array
  * of integers using the binary search algorithm.
  *
@@ -13,32 +36,32 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	unsigned int L = 0;
-	unsigned int R = size - 1;
-	unsigned int mid, i;
+	int L = 0;
+	int R = size - 1;
+	unsigned int mid;
 
 	if (array == NULL)
 		return (-1);
 
+	print_binary_search(array, L, R);
 	while (L <= R)
 	{
-		mid = (unsigned int)((L + R) / 2);
-
-		printf("Searching in array: ");
-		for (i = L; i <= R; i++)
-		{
-			printf("%i", i);
-			if (i != R)
-				printf(", ");
-		}
-		printf("\n");
+		mid = (L + R) / 2;
 
 		if (array[mid] < value)
+		{
 			L = mid + 1;
+			print_binary_search(array, L, R);
+		}
 		else if (array[mid] > value)
+		{
 			R = mid - 1;
+			print_binary_search(array, L, R);
+		}
 		else
+		{
 			return (mid);
+		}
 	}
 
 	return (-1);
