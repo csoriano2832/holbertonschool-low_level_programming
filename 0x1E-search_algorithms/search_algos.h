3d7e2193afc5d55924d8ed38c5755ac6c7889a8c
csoriano2832 <2832@holbertonschool.com>
2021-09-01 17:31:53 +0000

    add prototypes, macros,...

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,9 +3,21 @@
 
 /* STANDARD LIBRARIES */
 #include <stdio.h>
+#include <math.h>
+
+/* MACROS */
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
+/* ADVANCED */
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+/* modified binary search function for exponential search */
+int binary_search_2(int *array, int value, int left, int right);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
