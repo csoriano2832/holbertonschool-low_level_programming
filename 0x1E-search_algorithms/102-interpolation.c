5f8368ba1b731b08d588936caf5efbf5a564d95b
csoriano2832 <2832@holbertonschool.com>
2021-09-02 13:58:40 +0000

    Fix case: array is null

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -16,32 +16,33 @@ int interpolation_search(int *array, size_t size, int value)
 	int low = 0;
 	int high = size - 1;
 	size_t pos;
-	size_t index;
 
-	while (low <= high)
+	if (array == NULL)
+		return (-1);
+
+	while ((array[high] != array[low]) && (value >= array[low])
+			&& (value <= array[high]))
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 				* (value - array[low]));
 
-		if (pos > size)
-		{
-			printf("Value checked array[%lu] is out of range\n", pos);
-			return (-1);
-		}
-
 		printf("Value checked array[%lu] = [%i]\n", pos, array[pos]);
-		if (array[pos] == value)
-		{
-			index = pos;
-			break;
-		}
+		if (array[pos] < value)
+			low = pos + 1;
+		else if (value < array[pos])
+			high = pos - 1;
 		else
-		{
-			if (array[pos] < value)
-				low = pos + 1;
-			else
-				high = pos - 1;
-		}
+			return (pos);
 	}
-	return (index);
+
+	if (value == array[low])
+	{
+		return (low);
+	}
+	else
+	{
+
+		printf("Value checked array[%lu] is out of range\n", pos);
+		return (-1);
+	}
 }
